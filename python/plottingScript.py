52f49ee0cc78c4f8bce8efd5c5ab0f3f5dbf7b27
aferikoglou <aferikoglou@gmail.com>
2020-06-12 20:08:16 +0300

    Minor changes

--- python/plottingScript.py
+++ python/plottingScript.py
@@ -22,13 +22,13 @@ def png_gen(base_dir, simulation, path_to_sim_out_dir):
         if filename.startswith("POT_IT"):
             parts = filename.split("_")
             it = parts[2].split(".")[0]
-            command = """ gnuplot -e "set terminal png size 500,500 ; set output '""" + base_dir + """/""" + simulation + """/POT_IT_""" + it + """.png' ; set title 'Potential' ; set xlabel 'i' ; set xrange [-0.5:29.5] ; set ylabel 'j' ; set yrange [-0.5:29.5] ; set cbrange [-2.2:2.2] ;  plot [][] '""" + base_dir + """/""" + simulation + """/POT_IT_""" + it + """.dat' using 1:2:3 with image notitle" """
+            command = """ gnuplot -e "set terminal png size 500,500 ; set output '""" + base_dir + """/""" + simulation + """/POT_IT_""" + it + """.png' ; set title 'Potential' ; set xlabel 'i' ; set xrange [-0.5:99.5] ; set ylabel 'j' ; set yrange [-0.5:99.5] ; set cbrange [-2.2:2.2] ;  plot [][] '""" + base_dir + """/""" + simulation + """/POT_IT_""" + it + """.dat' using 1:2:3 with image notitle" """
             os.system(command)
         elif filename.startswith("MPV_HORCUT"):
-            command = """ gnuplot -e "set terminal png size 500,500 ; set output '""" + base_dir + """/""" + simulation + """/MPV_HORCUT.png' ; set title 'Mean Phase Velocity Horizontal Cut' ; set xlabel 'i' ; set xrange [-0.5:29.5] ; set ylabel 'Mean Phase Velocity' ;  plot '""" + base_dir + """/""" + simulation + """/MPV_HORCUT.dat' using 1:2 pointtype 7 notitle" """
+            command = """ gnuplot -e "set terminal png size 500,500 ; set output '""" + base_dir + """/""" + simulation + """/MPV_HORCUT.png' ; set title 'Mean Phase Velocity Horizontal Cut' ; set xlabel 'i' ; set xrange [-0.5:99.5] ; set ylabel 'Mean Phase Velocity' ;  plot '""" + base_dir + """/""" + simulation + """/MPV_HORCUT.dat' using 1:2 pointtype 7 notitle" """
             os.system(command)
         elif filename.startswith("MPV"):
-            command = """ gnuplot -e "set terminal png size 500,500 ; set output '""" + base_dir + """/""" + simulation + """/MPV.png' ; set title 'Mean Phase Velocity' ; set xlabel 'i' ; set xrange [-0.5:29.5] ; set ylabel 'j' ; set yrange [-0.5:29.5] ; plot [][] '""" + base_dir + """/""" + simulation + """/MPV.dat' using 1:2:3 with image notitle" """
+            command = """ gnuplot -e "set terminal png size 500,500 ; set output '""" + base_dir + """/""" + simulation + """/MPV.png' ; set title 'Mean Phase Velocity' ; set xlabel 'i' ; set xrange [-0.5:99.5] ; set ylabel 'j' ; set yrange [-0.5:99.5] ; plot [][] '""" + base_dir + """/""" + simulation + """/MPV.dat' using 1:2:3 with image notitle" """
             os.system(command)
 
 ##### Main script #####
